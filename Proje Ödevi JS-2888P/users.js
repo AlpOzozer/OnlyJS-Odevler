6ea52597e976888f65d0006313e89d12d767381f
AlpOzozer <64591442+AlpOzozer@users.noreply.github.com>
2025-12-04 15:20:03 +0300

    Add files via upload

---
+++ Proje Ödevi JS-2888P/users.js
@@ -0,0 +1,52 @@
+const header1 = document.createElement("h3");
+header1.id = "header1";
+const header2 = document.createElement("h3");
+header2.id = "header2";
+const header3 = document.createElement("h3");
+header3.id = "header3";
+const header4 = document.createElement("h3");
+header4.id = "header4";
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
+const p4 = document.createElement("p");
+const p5 = document.createElement("p");
+const p6 = document.createElement("p");
+const p7 = document.createElement("p");
+const p8 = document.createElement("p");
+const p9 = document.createElement("p");
+const p10 = document.createElement("p");
+const p11 = document.createElement("p");
+const p12 = document.createElement("p");
+const p13 = document.createElement("p");
+
+header1.innerHTML = "Temel Bilgiler";
+p1.innerHTML = "id";
+p2.innerHTML = "name";
+p3.innerHTML = "username";
+
+header2.innerHTML = "Adres Bilgileri";
+p4.innerHTML = "street";
+p5.innerHTML = "suite";
+p6.innerHTL = "city";
+p7.innerHTML = "zipcode";
+p8.innerHTML = "lat";
+p9.innerHTML = "lng";
+
+header3.innerHTML = "Şirket Bilgileri";
+p10.innerHTML = ""
+
+header4.innerHTML = "İletişim Bilgileri";
+
+
+
+document.getElementById("div-1").appendChild(header1);
+document.getElementById("div-1").appendChild(header2);
+document.getElementById("div-1").appendChild(header3);
+document.getElementById("div-1").appendChild(header4);
+document.getElementById("header1").appendChild(p1);
+document.getElementById("header2").appendChild(p2);
+document.getElementById("header3").appendChild(p3);
+document.getElementById("header4").appendChild(p4);
+
+document.getElementById()
