55adf808850a371a0ecd624bfb851fee6ca7ca4c
AlpOzozer <64591442+AlpOzozer@users.noreply.github.com>
2026-02-07 19:00:59 +0300

    commit

---
+++ src/sections/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import NavbarText1 from "../../../src/components/NavbarText1/NavbarText1";
+import NavbarText2 from "../../../src/components/NavbarText2/NavbarText2";
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
+const Navbar = function () {
+    return (
+        <>
+        <div className="navbar">
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <NavbarText1 />
+                    </Col>
+                    <Col md={6}>
+                        <NavbarText2 />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
