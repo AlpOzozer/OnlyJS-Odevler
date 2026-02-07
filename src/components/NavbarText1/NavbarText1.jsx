e2259d744f900147af9e2e00bb8028cf0ba307e1
AlpOzozer <64591442+AlpOzozer@users.noreply.github.com>
2026-02-07 19:15:09 +0300

    commit

--- src/components/NavbarText1/NavbarText1.jsx
+++
@@ -1,9 +0,0 @@
-const NavbarText1 = function () {
-    return (
-        <div className="row">
-            <a className="col-md-3 navbar-text-1">Start Bootstrap</a>
-        </div>
-    )
-}
-
-export default NavbarText1;
